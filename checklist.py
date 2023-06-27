28e3c584701238a5d849984028223249660938cf
Matheus Araujo <carvalhom630@gmail.com>
2023-06-26 22:58:45 -0300

    stylish part 1

--- checklist.py
+++ checklist.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, make_response, render_template, send_from_directory
 import sqlite3
 
 app = Flask(__name__)
+app.static_folder = 'static'
+
+@app.route('/static/css/<path:filename>')
+def serve_css(filename):
+    return send_from_directory(app.static_folder + '/css', filename)
 
 @app.route('/')
 def listar_projetos():
@@ -13,7 +18,12 @@ def listar_projetos():
 
     conn.close()
 
-    return render_template('lista_projetos.html', projetos=projetos)
+    response = make_response(render_template('lista_projetos.html', projetos=projetos))
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+
+    return response
 
 if __name__ == '__main__':
     app.run()
